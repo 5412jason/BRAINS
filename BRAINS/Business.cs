6b44b6b0ba6a957e9d52074b47cb683435de0835
5412jason <jasonhogan5412@gmail.com>
2019-12-02 21:02:25 -0500

    Created template windows for each window state.

--- BRAINS/Business.cs
+++ BRAINS/Business.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BRAINS
 {
-    public partial class Form1 : Form
+    public partial class Business : Form
     {
-        public Form1()
+        public Business()
         {
             InitializeComponent();
         }
