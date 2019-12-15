89c80d84e13c2774012a71e18760cd83aefd1577
Wojtas <tdwojtas@UMICH.EDU>
2019-12-15 18:38:30 -0500

    preview

---
+++ BRAINS/OversightPreview.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace BRAINS
+{
+    partial class OversightPreview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.previewTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // previewTextBox
+            // 
+            this.previewTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.previewTextBox.Location = new System.Drawing.Point(0, 0);
+            this.previewTextBox.Multiline = true;
+            this.previewTextBox.Name = "previewTextBox";
+            this.previewTextBox.ReadOnly = true;
+            this.previewTextBox.Size = new System.Drawing.Size(800, 450);
+            this.previewTextBox.TabIndex = 0;
+            // 
+            // OversightPreview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.previewTextBox);
+            this.Name = "OversightPreview";
+            this.Text = "OversightPreview";
+            this.Load += new System.EventHandler(this.OversightPreview_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox previewTextBox;
+    }
+}
