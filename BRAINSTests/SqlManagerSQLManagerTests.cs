467497fdf3e689ebfffbc494c19a1fa42b5d1957
Jason Hogan <jasonhogan5412@gmail.com>
2020-04-10 16:52:29 -0400

    Added unit test project.

---
+++ BRAINSTests/SqlManagerSQLManagerTests.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BRAINS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRAINS.Tests
+{
+    [TestClass()]
+    public class SqlManagerSQLManagerTests
+    {
+        [TestMethod()]
+        public void GetAllQuestionSetsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindQuestionSetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModifyQuestionSetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddQuestionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveQuestionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModifyQuestionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateNewQuestionSetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveQuestionSetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllUsersTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AuthenticateCredentialsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModifyUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllViolationsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDepartmentViolationsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddViolationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveViolationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllDepartmentsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveDepartmentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddDepartmentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUsersInDepartmentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindDepartmentByNameTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
